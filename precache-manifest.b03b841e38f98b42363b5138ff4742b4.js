7a82c7a1f3c07d24ba965d7e543614a47466eaa0
Nand Kishor <nandkk07@gmail.com>
2020-04-01 13:05:09 +0530

    Updates

---
+++ precache-manifest.b03b841e38f98b42363b5138ff4742b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "072d390bc6c515cc78466b729f4b99d4",
+    "url": "/Currency-Converter/index.html"
+  },
+  {
+    "revision": "fecd2a9fde456b301711",
+    "url": "/Currency-Converter/static/css/main.33d7be06.chunk.css"
+  },
+  {
+    "revision": "67840e1bfca0372ffc02",
+    "url": "/Currency-Converter/static/js/2.dc4f2694.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Currency-Converter/static/js/2.dc4f2694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecd2a9fde456b301711",
+    "url": "/Currency-Converter/static/js/main.bba163b0.chunk.js"
+  },
+  {
+    "revision": "f277d0a556ad41fa6a57",
+    "url": "/Currency-Converter/static/js/runtime-main.1b319497.js"
+  }
+]);
